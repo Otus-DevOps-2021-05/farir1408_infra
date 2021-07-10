a3d887acd6278962a64b28ee98af1fc40d644291
Oleg Venger <farir1881@gmail.com>
2021-07-10 23:01:12 +0300

    [HW-05] done homework.

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - # После пайпа sudo не убрано - а то не отработает добавление ключа
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+apt-get install -y apt-transport-https ca-certificates
+apt-get --assume-yes update
+apt-get --assume-yes install mongodb-org
+
+systemctl start mongod
+systemctl enable mongod
